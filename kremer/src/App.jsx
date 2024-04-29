ce412003fe1ba90ed7a95253ec98f36e9e23f370
Dara <darazadoroznaa2004@mail.ru>
2024-04-29 21:02:57 +0300

    стр с видео питон

--- kremer/src/App.jsx
+++ kremer/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 // import Header from './components/header'
 // import Footer from './components/footer'
 //import MainPage from './pages/main'
-import Registration from './pages/registration'
+//import Registration from './pages/registration'
+import Videocourse from './pages/videocourse'
 function App() {
   // const [count, setCount] = useState(0)y
   return (
     <div>
-  <Registration/>
+  
+  <Videocourse/>
     </div>
   )
 }
