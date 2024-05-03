0176d260654b116617e8736032a71002cb949da2
Sasha Solovev <sasha41187@gmail.com>
2024-05-03 15:05:57 +0300

    1

--- kremer/src/App.jsx
+++ kremer/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 // import Header from './components/header'
 // import Footer from './components/footer'
 import MainPage from './pages/main'
-
+import AsideIntro from './components/aside'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
-  <MainPage/>
+  <AsideIntro/>
     </div>
   )
 }
---
+++ kremer/src/components/aside.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// import '../styles/python_introduce.css'
+import '../styles/python_introduce.css'
+
+
+export default function AsideIntro(props){
+    return(
+        <aside className="aside-right" id="asidee">
+                  <div className="aside-stiky-wrapper">
+                    <div className="button__wrapper">
+                      <a href="#" className=" button__link">
+                      <button className="button__link-to-course" type="button" id="button__course">
+                      Записаться!
+                    </button>
+                    </a>
+                        
+                    </div>
+                    
+                  
+                  <div className="aside-right__main"> 
+                    <h4 className="aside-right__title">В данном курсе:</h4>
+                    <ul className="aside-right__list">
+                      <li className="aside-right__list-item">
+                        100500 уроков
+                      </li>
+                      <li className="aside-right__list-item">
+                        Миллион тестов
+                      </li>
+                      <li className="aside-right__list-item">
+                        Тысяси задач
+                      </li>
+                    </ul>
+                    <span className="aside-right__bottom1">
+                      Примерного время прохождения курса
+                    </span>
+                    <span className="aside-right__bottom2"> 50 часов</span>
+                  </div>
+                </div>
+                </aside>
+    )
+}
