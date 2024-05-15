36e1494d212b6713d1277283ec51bfbac28fb064
Dara <darazadoroznaa2004@mail.ru>
2024-05-15 23:16:37 +0300

    не рабочая версия

--- kremer/src/pages/python/lesson1/course_textPY.jsx
+++ kremer/src/pages/python/lesson1/course_textPY.jsx
@@ -7,88 +7,89 @@ import styles from "../../../styles/python/course_textPY.module.css";
 import arrow1_img from "../../../images/arrow1.png";
 import { Link } from 'react-router-dom';
 
-function Course_textPY() {
+function CourseTextPY() {
     return (
         <div>
             <Header />
-            <div className={styles.button_menu}>
-                                    <img src={arrow1_img} alt="" className={styles.arrow1_img} />
-                                </div>
-              <div className={styles["centered-text"]}>
-                 <p><strong>ЧИСЛА И ОПЕРАЦИИ НАД НИМИ</strong></p>
-                   <p>
+            <div className={styles.buttonMenu}>
+                <img src={arrow1_img} alt="" className={styles.arrow1_img} />
+            </div>
+            <div className={styles.centeredText}>
+                <p><strong>ЧИСЛА И ОПЕРАЦИИ НАД НИМИ</strong></p>
+                <p>
                     Добро пожаловать на курс "Python". Сегодня мы поговорим о числах и операциях
-                     над ними в Python. Python поддерживает различные типы чисел, такие как целые числа (int),
-                      числа с плавающей запятой (float) и комплексные числа. Давайте рассмотрим основные 
-                      операции, которые можно выполнять над этими числами.
-                    </p>
-        <pre>
-                a = 5
-                b = 3
-                print(a + b)
-        </pre>
-        <pre>
-              c = 3.14
-              d = 2.718 
-         </pre>
-              
-        <p>Теперь давайте посмотрим на операции, которые можно выполнять над этими числами:</p>
-        
-        <ul>
-                <li>Сложение (+) и Вычитание (-):
-                    <pre>
-                                            
-                    result_sum = a + b
-                    result_diff = c - d
-                        
-                    </pre>
-                </li>
+                    над ними в Python. Python поддерживает различные типы чисел, такие как целые числа (int),
+                    числа с плавающей запятой (float) и комплексные числа. Давайте рассмотрим основные 
+                    операции, которые можно выполнять над этими числами.
+                </p>
+                <pre className={styles.codeBlock}>
+                    {`
+                        a = 5
+                        b = 3
+                        print(a + b)
+                    `}
+                </pre>
+                <pre className={styles.codeBlock}>
+                    {`
+                        c = 3.14
+                        d = 2.718 
+                    `}
+                </pre>
+                <p>Теперь давайте посмотрим на операции, которые можно выполнять над этими числами:</p>
+                <ul>
+                    <li>
+                        Сложение (+) и Вычитание (-):
+                        <pre className={styles.codeBlock}>
+                            {`
+                                result_sum = a + b
+                                result_diff = c - d
+                            `}
+                        </pre>
+                    </li>
+                    <li>
+                        Умножение (*) и Деление (/):
+                        <pre className={styles.codeBlock}>
+                            {`
+                                result_mult = a * c
+                                result_div = b / d
+                            `}
+                        </pre>
+                    </li>
+                    <li>
+                        Целочисленное деление (//) и Остаток от деления (%):
+                        <pre className={styles.codeBlock}>
+                            {`
+                                result_floor_div = a // b
+                                result_modulo = a % b
+                            `}
+                        </pre>
+                    </li>
+                    <li>
+                        Возведение в степень () и Квадратный корень (math.sqrt()):
+                        <pre className={styles.codeBlock}>
+                            {`
+                                import math
 
-                <li>Умножение (*) и Деление (/):
-                    <pre>
-                                            
-                    result_mult = a * c
-                    result_div = b / d
-                        
-                    </pre>
-                </li>
-
-                <li>Целочисленное деление (//) и Остаток от деления (%):
-                    <pre>
-                                            
-                    result_floor_div = a // b
-                    result_modulo = a % b
-                        
-                    </pre>
-                </li>
-
-                <li>Возведение в степень () и Квадратный корень (math.sqrt()):
-                    <pre>
-                        
-                    import math
-
-                    result_power = a ** 2
-                    result_sqrt = math.sqrt(b)
-                        
-                    </pre>
-                </li>
-            </ul>
-            <p>Это лишь малая часть операций, которые можно выполнять над числами в Python. Помните, что Python также предоставляет множество встроенных функций для работы с числами.</p>
-
-            <a href="pdf/python1.pdf" download>Скачать материал</a>
-                </div>
-             <div className={styles["step-buttons-container"]}>
-                  <Link to="/videocoursePY1">
-                     <button className={styles["step-button"]}>Назад</button>
-                   </Link>
-                   <Link to="/videocoursePY2">
-                  <button className={styles["step-button"]}>Вперед</button>
-                  </Link>
-             </div>
+                                result_power = a ** 2
+                                result_sqrt = math.sqrt(b)
+                            `}
+                        </pre>
+                    </li>
+                </ul>
+                <p>Это лишь малая часть операций, которые можно выполнять над числами в Python. Помните, что Python также предоставляет множество встроенных функций для работы с числами.</p>
+                <a href="pdf/python1.pdf" download>Скачать материал</a>
+            </div>
+            <div className={styles.stepButtonsContainer}>
+                <Link to="/videocoursePY1" className={styles.stepButton}>
+                    <button>Назад</button>
+                </Link>
+                <Link to="/videocoursePY2" className={styles.stepButton}>
+                    <button>Вперед</button>
+                </Link>
+            </div>
             <Footer />
         </div>
-        
     );
 }
  
-export default Course_textPY;
+export default CourseTextPY;
--- kremer/src/pages/python/lesson1/videocoursePY.jsx
+++ kremer/src/pages/python/lesson1/videocoursePY.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from "../../../components/headerPY";
 import Footer from "../../../components/footerPY";
-import styles from "../../../styles/python/VideocoursePY.module.css"; 
+import styles from "../../../styles/python/videocoursePY.module.css"; 
 import python from "../../../images/python.mp4";
 import { Link } from 'react-router-dom';
 
@@ -20,46 +20,40 @@ function VideocoursePY() {
             </button>
             
             {isPanelOpen && (
-    <div className={styles.panel}>
-        <a>ВВЕДЕНИЕ В КУРС</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a> Базовые операции в языке Python</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a> Краткая теория</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a> Задачи</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a>Переменные и типы данных Python</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a> Краткая теория</a>
-        <hr /> {/* Горизонтальная линия */}
-        <a> Задачи</a>
-        <hr /> {/* Горизонтальная линия */}
-    </div>
-)}
+                <div className={styles.panel}>
+                    <a>ВВЕДЕНИЕ В КУРС</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a> Базовые операции в языке Python</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a> Краткая теория</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a> Задачи</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a>Переменные и типы данных Python</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a> Краткая теория</a>
+                    <hr /> {/* Горизонтальная линия */}
+                    <a> Задачи</a>
+                    <hr /> {/* Горизонтальная линия */}
+                </div>
+            )}
 
             <main className={styles.main}>
-                
-            
                 <section className={styles.top}>
                     <div className={styles.container1}>
                         <div className={styles.top__wrapper}>
                             <div className={styles.zaeb}>
-                                
                                 <h1 className={styles.greetings}>ВВЕДЕНИЕ В КУРС</h1>
-                                
                                 <div className={styles["content-wrapper"]}>
-                            
                                     <video controls className={styles.video}>
                                         <source src={python} type="video/mp4" />
                                         Ваш браузер не поддерживает видео в формате MP4.
                                     </video>
                                 </div>
                                 <div className={styles["step-buttons-container"]}>
-                                    <button className={styles["step-button"]}>Назад</button> {/* будет вести на стр с инфой про курс????*/}
-
+                                    <button className={styles["step-button"]}>Назад</button>
                                     <Link to="/videocoursePY1">
-                                    <button className={styles["step-button"]}>Далее</button>
+                                        <button className={styles["step-button"]}>Далее</button>
                                     </Link>
                                 </div>
                             </div>
